b4b4b7db930325ab5844a68797a7825b86540409
AraicheKhalil <ucandevlopeit@gmail.com>
2024-01-14 21:27:41 +0100

    new sec

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 require('express-async-errors')
 
 // extra security packages
-// const helmet = require('helmet');
-// const xss = require('xss-clean');
-// const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
 const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const express = require('express')
 const app = express()
@@ -17,27 +17,39 @@ const notFound = require('./middlewares/notFound')
 const errorHandler_Middleware = require('./middlewares/error-handler')
 
 
-app.use(express.json())
-// const corsOptions = {
-//     origin: 'http://localhost:3000',  // replace with your frontend URL
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-// };
+// app.use(express.json())
+// // const corsOptions = {
+// //     origin: 'http://localhost:3000',  // replace with your frontend URL
+// //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// //     credentials: true,
+// // };
+// app.use(cors());
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
 app.use(cors());
-
+app.use(xss());
 
 app.use('/api/v1/workouts',workoutRouter)
 app.use('/api/v1/auth',authRouter)
+app.get('/',(req,res) => {
+    res.send('hi server hosted')
+})
 
 app.use("*",notFound)
 app.use(errorHandler_Middleware)
 
 
 
-
 const start = async () => {
     try {
-        connectToDB(process.env.MONGO_URL)
+        await connectToDB(process.env.MONGO_URL)
         app.listen(process.env.PORT || 3030 , () => {
             console.log(`the server run on the PORT ${process.env.PORT}`);
         })
